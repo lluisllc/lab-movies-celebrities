bfdfbdfc847f343ab0324acde3e3543c44f7338d
lluisllc <lluis.llacera@gmail.com>
2022-06-28 16:36:15 +0200

    Iteración 7
    Iteración 7

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -4,5 +4,28 @@ const Movie = require('./../models/Movie.model');
 require('../db');
 
 // all your routes here
+router.get('/movies/create', (req, res, next) => res.render('movies/new-movie'));
+
+
+router.post('/movies/create', (req, res, next) => {
+    console.log(req.body);
+    const { title, genre, plot, cast } = req.body;
+
+    Movie.create({ title, genre, plot, cast })
+
+        .then(() => res.redirect('/movies'))
+        .catch(error => res.render('movies/new-movie'));
+});
+
+router.get('/movies', (req, res, next) => {
+    // List the movies
+    Movie.find()
+        .then(response => {
+            console.log("hola", { response });
+            res.render("movies/movies.hbs", { response });
+        })
+        .catch(error => console.log(error));
+});
+
 
 module.exports = router;
